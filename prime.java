62134ad19b876c25097d399a139811d1eaf75c7e
HYPER_77 <143863603+Hyper7711@users.noreply.github.com>
2025-09-04 19:40:51 +0530

    update prime

--- prime.java
+++ prime.java
@@ -1,24 +1,37 @@
+import java.util.Scanner;
+
 public class prime {
     public static void isprime(int n) {
      
         boolean check=true;
 
 
-       for(int i=2;i*i<=n;i++ ){
+       for(int i=2;i<=n-1;i++){
         if(n%i==0){
             check=false;
             break;
         }
        }
 
-        if(check==true){
+        if(check == true){
             System.out.println("number is prime");
 
         }else{
              System.out.println("number is not prime");
         }
 
+      
         
     }
+      public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a NUM");
+        int n = sc.nextInt();
+        isprime(n);
+
+            
+        }
+
 }   
 
