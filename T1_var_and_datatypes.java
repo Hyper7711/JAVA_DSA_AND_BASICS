85b76178327e6553f731dd3fcca247c877fb3487
HYPER_77 <143863603+Hyper7711@users.noreply.github.com>
2025-09-04 20:40:16 +0530

    topic 1 - variables and datatypes ... completed

---
+++ T1_var_and_datatypes.java
@@ -0,0 +1,17 @@
+public class T1_var_and_datatypes {
+    public static void main(String[] args) {
+        
+        int age = 21;
+        double height = 5.7;
+        char grade = 'A';
+        Boolean ismale = true;
+        String name = "YASH SHINDE";
+
+        System.out.println("NAME: " + name);
+        System.out.println("AGE: " + age);
+        System.out.println("HEIGHT ?: " + height);
+        System.out.println("MALE ?: " + ismale);
+        System.out.println("GRADE ?: " + grade);
+        
+    }
+}
--- prime.java
+++ prime.java
@@ -6,7 +6,7 @@ public class prime {
         boolean check=true;
 
 
-       for(int i=2;i<=n-1;i++){
+       for(int i=2;i<=Math.sqrt(n);i++){
         if(n%i==0){
             check=false;
             break;
