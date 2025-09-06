97cafee3c44d95b80245e61e6986b93c0fa2a189
HYPER_77 <143863603+Hyper7711@users.noreply.github.com>
2025-09-06 22:26:59 +0530

    operaters_commit

--- operators.java
+++ operators.java
@@ -11,14 +11,42 @@ public class operators {
         System.out.println("Enter the second number: ");
         int b = sc.nextInt();
 
-        System.out.println("+++++ Arthematic +++++" );
+        System.out.println("\n+++++ Arthematic +++++" );
         System.out.println("( a + b ) = " + (a + b));
         System.out.println("( a - b ) = " + (a - b));
         System.out.println("( a * b ) = " + (a * b));
         System.out.println("( a / b ) = " + (a / b));
         System.out.println("( a % b ) = " + (a % b));
 
+        System.out.println("\n+++++ Relational +++++");
+        System.out.println("a == b? " +  (a == b));
+        System.out.println("a != b? " +  (a != b));
+        System.out.println("a > b?  " +  (a > b));
+        System.out.println("a < b? " +  (a < b));
+        System.out.println("a >= b " +  (a >= b));
+        System.out.println("a <= b " +  (a <= b));
 
+        System.out.println("\n+++++ Logical +++++");
+        boolean cond1 = (a > 0);
+        boolean cond2 = (a > 0);
+        System.out.println("Both positive" + (cond1 && cond2));
+        System.out.println("At one positive? " + (cond1 || cond2));
+        System.out.println("a is not positive? " + (!cond1));
+
+        System.out.println("\n+++++ Assignment +++++");
+        int x = a;
+        x += b;
+        System.out.println("x += b" + x);
+        x -= b;
+        System.out.println("x -= b" + x);
+
+        System.out.println("\n+++++ Unary +++++");
+        int y = a;
+        System.out.println("y++ " + (y++));
+        System.out.println("After y++, y = " +  y);
+        System.out.println("++y = " + (++y));
+
+        sc.close();
 
     }
 }
