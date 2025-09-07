5da615cbd07fc2fdd818a6db8322b18dc66bb324
HYPER_77 <143863603+Hyper7711@users.noreply.github.com>
2025-09-07 21:01:36 +0530

    Butterfly_pattern

---
+++ Pattern.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Pattern {
+
+    
+    public static void Butterfly(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+             for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+            for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+             for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter A NUM");
+        int n = sc.nextInt();
+
+        Butterfly(n);
+        
+    }
+    
+}
