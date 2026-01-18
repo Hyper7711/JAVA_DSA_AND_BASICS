a9bf1393185e6aef6c9b8b658bd0407d0503340f
Hyper7711 <shindeyash054@gmail.com>
2026-01-18 08:56:18 +0530

    searches_vaiation

--- Searches.java
+++ Searches.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class Searches {
     public static void main(String[] args){
 
-        //Linear search
+        // Linear search
 
         int arr[] = {1, 2, 3, 6, 8, 9};
         int key = 8;
@@ -12,5 +14,90 @@ public class Searches {
                 break;
             }
         }
+
+        // Linear search variation - 1 :
+
+        int ARR2[] = {11, 22, 33, 44, 55, 66};
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the required element - ");
+        int key2 = sc.nextInt();
+
+        boolean found = false;
+
+
+        for(int i = 0; i < ARR2.length; i++){
+            if(ARR2[i] == key2){
+                System.out.println("Element found at index = " + i);
+            found = true;
+            break;
+            }
+        }
+
+        if(found == false){
+            System.out.println("NO such element is present");
+        }
+
+        sc.close();
+
+
+        // Linear search variation - 2 :
+
+        int ARR3 [] = {77, 88, 99, 00, 12, 13, 14, 15};
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Enter the Element to count - ");
+        int key3 = sc2.nextInt();
+
+        int count = 0;
+
+        for(int i = 0; i < ARR3.length; i++){
+            if(ARR3[i] == key3){
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("Element not found");
+        }else{
+            System.out.println("Element appears" + count + "times");
+        }
+
+        sc2.close();
+
+        // linear variation - 3 :
+
+        Scanner sc3 = new Scanner(System.in);
+
+        
+        System.out.println("Enter size of array");
+        int n = sc3.nextInt();
+
+        int ARR4[] = new int[n];
+
+        System.out.println("Enter the elements of array");
+        for(int i = 0; i < n; i++){
+            ARR4[i] = sc3.nextInt();
+        }
+
+        System.out.println("Enter element to search");
+        int key4 = sc3.nextInt();
+
+        boolean found2 = false;
+
+        for(int i = 0;i < ARR4.length; i++){
+            if(ARR4[i] == key4){
+                System.out.println("Element found at - " + i);
+                found2 = true;
+                break;
+            }
+        }
+
+        if(found2 == false){
+            System.out.println("Element not found");
+        }
+
+        sc3.close();
+
     }
 }
