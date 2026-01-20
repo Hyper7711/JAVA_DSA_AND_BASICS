569b6acf2e2c08f1c6eb8cbfea8780c467b8920b
Hyper7711 <shindeyash054@gmail.com>
2026-01-20 11:16:04 +0530

    Binary_Search_1

---
+++ Binary_search.java
@@ -0,0 +1,24 @@
+public class Binary_search {
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 6, 8, 10, 12};
+        int key = 8;
+
+        int low = 0;
+        int high = arr.length - 1;
+        
+        while(low <= high){
+
+            int mid = (low + high) / 2;
+
+            if(mid == key){
+                System.out.println("Element found at index = " + mid);
+            }
+            else if(key < arr[mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+    }  
+}
