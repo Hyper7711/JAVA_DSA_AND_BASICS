3325daf99a31c3c75b772b606770c52e444955ac
Hyper7711 <shindeyash054@gmail.com>
2026-01-23 10:11:45 +0530

    commit_3

--- BinarySearchUserArray.java
+++ BinarySearchUserArray.java
@@ -5,28 +5,28 @@ public class BinarySearchUserArray {
 
         Scanner sc = new Scanner(System.in);
 
-        // Step 1: Array size
+
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
 
-        // Step 2: Array elements (sorted)
+
         System.out.println("Enter array elements in SORTED order:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        // Step 3: Key to search
+
         System.out.print("Enter element to search: ");
         int key = sc.nextInt();
 
-        // Step 4: Binary search variables
+
         int low = 0;
         int high = arr.length - 1;
         boolean found = false;
 
-        // Step 5: Binary search logic
+
         while (low <= high) {
 
             int mid = (low + high) / 2;
@@ -44,7 +44,7 @@ public class BinarySearchUserArray {
             }
         }
 
-        // Step 6: Not found case
+
         if (!found) {
             System.out.println("Element not found");
         }
