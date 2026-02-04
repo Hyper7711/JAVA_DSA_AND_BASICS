f6227c3e0a2374d7be1d07e6c44c7f7303f02ff4
Hyper7711 <shindeyash054@gmail.com>
2026-02-04 10:48:44 +0530

    Add_Comments

--- BubbleSort.java
+++ BubbleSort.java
@@ -10,16 +10,19 @@ public class BubbleSort {
 
                 if(arr[i] > arr[i + 1]){
 
+                    // swap
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
                 }
             }
         }
+
+        // print array
+
         for(int i = 0; i < n;i++){
             System.out.print(arr[i] + " ");
         }
-
     }
 }
 
