6b9ec181c21c78b5ceea604c53c39c9793236138
Hyper7711 <shindeyash054@gmail.com>
2026-02-04 10:29:08 +0530

    bubble_sort+SelectionSort

--- BubbleSort_2.java
+++ BubbleSort_2.java
@@ -1,7 +1,7 @@
 public class BubbleSort_2 {
-    public static void main(String[] args) {
-        int[] arr = {5,3,8,2};
 
+    static void BubbleSort(int arr[]){
+        
         for(int i = 0; i<arr.length-1;i++){
             for(int j = 0 ; j<arr.length-i-1;j++){
                 if(arr[j]>arr[j+1]){
@@ -11,10 +11,46 @@ public class BubbleSort_2 {
                 }
             }
         }
+        
+    }
 
+    static void SelectionSort(int arr[]){
+        int l=arr.length;
+        print(arr);
+        System.out.println();
+        for(int i = 0; i<l-1;i++){
+            int minIndex = i;
+            for(int j = i+1 ; j<l;j++){
+                if(arr[minIndex]>arr[j]){
+                    minIndex=j;
+                }
+            }
+            //swap
+            int temp = arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+
+            print(arr);
+            System.out.println();
+        }
+    }
+
+    
+
+    static void print(int arr[]){
+            
         for(int i =0;i<arr.length;i++){
             System.out.print(arr[i] + " ");
         }
+
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,3,8,2};
+
+        SelectionSort(arr);
+        print(arr);
+        
+       
     }
     
 }
