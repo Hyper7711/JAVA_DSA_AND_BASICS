64f73edd742d2b49863e71597cf9c3b4a0c8f577
Hyper7711 <shindeyash054@gmail.com>
2026-07-18 15:26:17 +0530

    hollowSquare

--- Pattern.java
+++ Pattern.java
@@ -25,6 +25,19 @@ public class Pattern {
         }
     }
 
+    public static void HollowSquare(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == 1 || i == n || j == 1 || j == n){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     // Butterfly Pattern
     public static void Butterfly(int n){
         // Upper part
