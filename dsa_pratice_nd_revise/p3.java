281a51169a7b988ecca0b388d7be66d6aed9fb62
Hyper7711 <shindeyash054@gmail.com>
2026-07-18 15:26:29 +0530

    praticeDay_2

--- dsa_pratice_nd_revise/p3.java
+++ dsa_pratice_nd_revise/p3.java
@@ -6,15 +6,18 @@ public class p3 {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int m = sc.nextInt();
 
-     
+        for (int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
 
-        for(int i = 1; i < 11; i++){
-
-            System.out.println(n*i);
-        
+                if(i == 1 || j == 1 || i == n || j == m){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
-
-
     }
 }
