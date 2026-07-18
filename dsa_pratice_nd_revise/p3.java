1407c4ff238f0746ae8bc31d989c61537fae5593
Hyper7711 <shindeyash054@gmail.com>
2026-07-18 16:15:27 +0530

    add_invertedPyramid

--- dsa_pratice_nd_revise/p3.java
+++ dsa_pratice_nd_revise/p3.java
@@ -6,18 +6,12 @@ public class p3 {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = sc.nextInt();
 
-        for (int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-
-                if(i == 1 || j == 1 || i == n || j == m){
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+        for (int i = n; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 }
