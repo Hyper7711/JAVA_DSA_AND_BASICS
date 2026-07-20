fa394442986828329cd0ac34722973114964bc6f
Hyper7711 <shindeyash054@gmail.com>
2026-07-20 12:53:22 +0530

    pratice_day_3

--- dsa_pratice_nd_revise/p3.java
+++ dsa_pratice_nd_revise/p3.java
@@ -6,12 +6,58 @@ public class p3 {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int num = 1;
 
-        for (int i = n; i >= 1; i--){
+
+        inverted half pyramid(rotate180 degree)- 
+
+        for (int i = 1; i <= n; i++){
+
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
             for(int j = 1; j <= i; j++){
                 System.out.print("*");
             }
             System.out.println("");
         }
+
+        inverted half pyramid with numbers
+
+        for(int i = 1; i <= n; i ++){
+
+            for(int j = 1; j <= n - i + 1; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        Floyd's triangle - 
+
+        for(int i = 1; i <= n ; i++){
+
+            for(int j = 1; j <= i; j++){
+                System.out.print(num+" ");
+                num = num + 1;
+            }
+            System.out.println();
+        }
+
+        0-1 Triangle
+        for(int i = 1; i <= n; i++){
+            for(int j = 1;j <= i; j++){
+                int sum = i + j;
+
+                if(sum % 2 == 0){
+                System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+
+    sc.close();
     }
 }
