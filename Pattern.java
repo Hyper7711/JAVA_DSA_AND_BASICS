6e086d9520b7e42ca56e080a7aae8805656118b3
Hyper7711 <shindeyash054@gmail.com>
2026-07-20 12:53:56 +0530

    added_newpatterns

--- Pattern.java
+++ Pattern.java
@@ -25,6 +25,7 @@ public class Pattern {
         }
     }
 
+    // Hollow Square
     public static void HollowSquare(int n){
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
@@ -38,14 +39,13 @@ public class Pattern {
         }
     }
 
-    //Inverted Pattern
-  
-    public static void InvertedTriangle (int n){
+    // Inverted Triangle
+    public static void InvertedTriangle(int n){
         for(int i = n; i >= 1; i--){
             for(int j = 1; j <= i; j++){
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 
@@ -80,6 +80,56 @@ public class Pattern {
         }
     }
 
+    // Inverted Half Pyramid (Rotated 180 Degrees)
+    public static void InvertedHalfPyramid180(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // Inverted Half Pyramid with Numbers
+    public static void InvertedHalfPyramidNumbers(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i + 1; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    // Floyd's Triangle
+    public static void FloydTriangle(int n){
+        int num = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    // 0-1 Triangle
+    public static void ZeroOneTriangle(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                int sum = i + j;
+                if(sum % 2 == 0){
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     // Main Method (Menu Driven)
     public static void main(String[] args) {
 
@@ -89,12 +139,16 @@ public class Pattern {
         System.out.println("1. Square");
         System.out.println("2. Pyramid");
         System.out.println("3. Butterfly");
-        System.out.println("4. INvertedTriangle");
-        System.out.println("5. Pyramid");
+        System.out.println("4. Inverted Triangle");
+        System.out.println("5. Hollow Square");
+        System.out.println("6. Inverted Half Pyramid (180 Degree)");
+        System.out.println("7. Inverted Half Pyramid with Numbers");
+        System.out.println("8. Floyd's Triangle");
+        System.out.println("9. 0-1 Triangle");
 
         int choice = sc.nextInt();
 
-        System.out.println("Enter a number:");
+        System.out.print("Enter a number: ");
         int n = sc.nextInt();
 
         switch(choice){
@@ -111,10 +165,24 @@ public class Pattern {
                 InvertedTriangle(n);
                 break;
             case 5:
-                Pyramid(n);
+                HollowSquare(n);
+                break;
+            case 6:
+                InvertedHalfPyramid180(n);
+                break;
+            case 7:
+                InvertedHalfPyramidNumbers(n);
+                break;
+            case 8:
+                FloydTriangle(n);
+                break;
+            case 9:
+                ZeroOneTriangle(n);
+                break;
             default:
                 System.out.println("Invalid Choice!");
         }
+
         sc.close();
     }
 }
