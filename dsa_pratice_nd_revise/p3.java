9b74e3bb1d5867266d9c0caaa8b6f52fce326bd5
Hyper7711 <shindeyash054@gmail.com>
2026-07-21 14:27:44 +0530

    optimize

--- dsa_pratice_nd_revise/p3.java
+++ dsa_pratice_nd_revise/p3.java
@@ -6,58 +6,39 @@ public class p3 {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int num = 1;
 
-
-        inverted half pyramid(rotate180 degree)- 
-
-        for (int i = 1; i <= n; i++){
+        for(int i = 1; i <= n; i++){
 
             for(int j = 1; j <= n - i; j++){
                 System.out.print(" ");
             }
-            for(int j = 1; j <= i; j++){
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
 
-        inverted half pyramid with numbers
-
-        for(int i = 1; i <= n; i ++){
-
-            for(int j = 1; j <= n - i + 1; j++){
-                System.out.print(j);
-            }
-            System.out.println();
-        }
-
-        Floyd's triangle - 
-
-        for(int i = 1; i <= n ; i++){
-
-            for(int j = 1; j <= i; j++){
-                System.out.print(num+" ");
-                num = num + 1;
-            }
-            System.out.println();
-        }
-
-        0-1 Triangle
-        for(int i = 1; i <= n; i++){
-            for(int j = 1;j <= i; j++){
-                int sum = i + j;
-
-                if(sum % 2 == 0){
-                System.out.print("1");
-                }
-                else{
-                    System.out.print("0");
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
                 }
             }
+
             System.out.println();
         }
 
-    sc.close();
+        for(int i = n - 1; i >= 1; i--){
+
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
     }
 }
