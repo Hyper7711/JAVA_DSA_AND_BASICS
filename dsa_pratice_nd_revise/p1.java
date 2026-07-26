b9c8bb1f54cebfa5e1f78fa6e5783ec880505cf0
Hyper7711 <shindeyash054@gmail.com>
2026-07-26 12:49:56 +0530

    PraticeDay7

--- dsa_pratice_nd_revise/p1.java
+++ dsa_pratice_nd_revise/p1.java
@@ -1,33 +1,11 @@
 class p1 {
 
-    public static void maxrem(int n){
-
-        int num =0;
-        int max = 0;
-
-
-        for(int i=1;i*i<=n;i++){
-
-            int rem = n%i;
-
-            if(rem>max){
-                max=rem;
-                num=i;
-            }
-                i++;
-            
-        }
-
-        System.out.println(num);
-        System.out.println(max);
-     
-
-    } 
-
-
     public static void main(String args[]){
-        int n=87;
-        maxrem(n);
+
+        int array[] = {11,22,33,44,55,66};
+
+        System.out.println(array[2]);
+
     }
     
 }
--- dsa_pratice_nd_revise/p3.java
+++ dsa_pratice_nd_revise/p3.java
@@ -2,19 +2,32 @@ import java.util.*;
 
 public class p3{
 
+    // Declaring array
+
+    // int array[] = new int[3];
+    // int array2[] = {11,22,33,44,55};
+
     public static void main(String args[]){
 
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // int size = sc.nextInt();
 
-        int num[] = new int[size];
+        // int num[] = new int[size];
 
-        for(int i = 0; i < size; i ++){
-            num [i] = sc.nextInt();
-        }
+        // for(int i = 0; i < size; i ++){
+        //     num [i] = sc.nextInt();
+        // }
 
-        for(int i  = 0; i < num.length; i ++){
-            System.out.println(num [i] + " ");
+        // for(int i  = 0; i < num.length; i ++){
+        //     System.out.println(num [i] + " ");
+        // }
+
+        int array[] = {11,22,33,44,55,66};
+
+        System.out.println(array[2]);
+
+        // for(int i = 0; i < array.length; i ++){
+        //     System.out.println(array[i]);
         }
 
     }
