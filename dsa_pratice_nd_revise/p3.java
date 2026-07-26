a7e25be1eb4ba2d35d1087b0d170ea7073849b92
Hyper7711 <shindeyash054@gmail.com>
2026-07-26 12:37:50 +0530

    PraticeDay7

--- dsa_pratice_nd_revise/p3.java
+++ dsa_pratice_nd_revise/p3.java
@@ -1,50 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class p3{
 
-public static void main(String arg[]){
-    Scanner sc = new Scanner(System.in);
+    public static void main(String args[]){
 
-    int pos = 0;
-    int neg = 0;
-    int zero = 0;
-    char ch;
-    
-    do{
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
 
-        int num = sc.nextInt();
+        int num[] = new int[size];
 
-        if(num > 0){
-            pos++;
-        }
-        else if (num < 0){
-            neg++;
-        }else{
-            zero++;
+        for(int i = 0; i < size; i ++){
+            num [i] = sc.nextInt();
         }
 
-        System.out.println("continue ? ");
-        ch = sc.next().charAt(0);    //imp line 
-
-    }while(ch == 'y' );
-
-<<<<<<< HEAD
-        System.out.println("number of pos " + pos);
-        System.out.println("number of neg " + neg);
-        System.out.println("number is zero "+ zero);
-}
-}
-=======
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-
-            System.out.println();
+        for(int i  = 0; i < num.length; i ++){
+            System.out.println(num [i] + " ");
         }
+
     }
-    
->>>>>>> 49c6222f08092887a103fefe4e54d0e2a6e658df
+}
