98430d9253e21f1480f57d440b8cbdf6e05ebcb7
Hyper7711 <shindeyash054@gmail.com>
2026-07-26 12:50:05 +0530

    .,..

--- dsa_pratice_nd_revise/p3.java
+++ dsa_pratice_nd_revise/p3.java
@@ -9,25 +9,21 @@ public class p3{
 
     public static void main(String args[]){
 
-        // Scanner sc = new Scanner(System.in);
-        // int size = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
 
-        // int num[] = new int[size];
+        int num[] = new int[size];
 
-        // for(int i = 0; i < size; i ++){
-        //     num [i] = sc.nextInt();
-        // }
+        for(int i = 0; i < size; i ++){
+            num [i] = sc.nextInt();
+        }
 
-        // for(int i  = 0; i < num.length; i ++){
-        //     System.out.println(num [i] + " ");
-        // }
+        for(int i  = 0; i < num.length; i ++){
+            System.out.println(num [i] + " ");
+        }
 
-        int array[] = {11,22,33,44,55,66};
-
-        System.out.println(array[2]);
-
-        // for(int i = 0; i < array.length; i ++){
-        //     System.out.println(array[i]);
+        for(int i = 0; i < array.length; i ++){
+            System.out.println(array[i]);
         }
 
     }
