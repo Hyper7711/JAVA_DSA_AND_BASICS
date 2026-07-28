58b8d815d8c2db080d81b0e099f878d9392d4995
Hyper7711 <shindeyash054@gmail.com>
2026-07-28 21:31:25 +0530

    array_initialize_revise

--- dsa_pratice_nd_revise/p3.java
+++ dsa_pratice_nd_revise/p3.java
@@ -2,29 +2,18 @@ import java.util.*;
 
 public class p3{
 
-    // Declaring array
-
-    // int array[] = new int[3];
-    // int array2[] = {11,22,33,44,55};
-
     public static void main(String args[]){
-
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
+        int n = sc.nextInt();
 
-        int num[] = new int[size];
+        int num[] = new int[n];
 
-        for(int i = 0; i < size; i ++){
-            num [i] = sc.nextInt();
+        for(int i = 0; i < n; i ++){
+            num[i] = sc.nextInt();
         }
 
-        for(int i  = 0; i < num.length; i ++){
-            System.out.println(num [i] + " ");
+        for(int i = 0; i < num.length; i++){
+            System.out.print(num[i]);
         }
-
-        for(int i = 0; i < num.length; i ++){
-            System.out.println(num[i]);
-        }
-
     }
 }
